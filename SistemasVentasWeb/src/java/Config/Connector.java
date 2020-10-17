b14387e2fc76b2015734a88282c33aac2eabe216
jpromanonet <programming@jpromano.net>
2020-10-17 16:26:50 -0700

    Connection

--- SistemasVentasWeb/src/java/Config/Connector.java
+++ SistemasVentasWeb/src/java/Config/Connector.java
@@ -6,6 +6,8 @@ import java.sql.DriverManager;
 public class Connector {
     Connection con;
     String url="jdbc:mysql://localhost:3306/bd_ventas";
+    String user="root";
+    String pass="";
     public Connection Connector(){
         try {
             Class.forName("com.mysql.jdbc.Driver");
