3660dba6d7ec3d901db14b5c4693d180060dcb27
jpromanonet <programming@jpromano.net>
2020-10-21 16:17:18 -0700

    Empleado DAO

--- SistemasVentasWeb/src/java/Controller/Validator.java
+++ SistemasVentasWeb/src/java/Controller/Validator.java
@@ -1,10 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Controller;
 
+import Model.Empleado;
+import Model.EmpleadoDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -18,15 +15,9 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class Validator extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    EmpleadoDAO edao = new EmpleadoDAO();
+    Empleado em = new Empleado();
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
