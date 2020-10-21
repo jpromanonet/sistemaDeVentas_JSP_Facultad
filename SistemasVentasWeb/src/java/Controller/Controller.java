7add997d45e65df99218992eb7441f4ed2e95ac3
jpromanonet <programming@jpromano.net>
2020-10-21 16:31:13 -0700

    Empleado DAO

--- SistemasVentasWeb/src/java/Controller/Controller.java
+++ SistemasVentasWeb/src/java/Controller/Controller.java
@@ -29,18 +29,14 @@ public class Controller extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Controller</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Controller at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            String accion = request.getParameter("accion");
+            switch(accion){
+                case "Principal":
+                    request.getRequestDispatcher("Principal.jsp").forward(request, response);
+                    break;
+                default:
+                    throw new AssertionError();
+            }
         }
     }
 
