e1f975777242fef618acb2ba92f762b81a1dba0b
jpromanonet <programming@jpromano.net>
2020-10-21 16:03:02 -0700

    Empleado DAO

--- SistemasVentasWeb/src/java/Model/EmpleadoDAO.java
+++ SistemasVentasWeb/src/java/Model/EmpleadoDAO.java
@@ -1,7 +1,13 @@
 package Model;
 
 import Config.Connector;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class EmpleadoDAO {
     Connector cn=new Connector();
+    Connection con;
+    PreparedStatement ps;
+    ResultSet rs;
 }
