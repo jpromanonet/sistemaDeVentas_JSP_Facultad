815cd2471b30e70408689bd5bdacd4408ac73f68
jpromanonet <programming@jpromano.net>
2020-11-05 07:22:28 -0800

    Controlador Cliente

--- sistemaVentasWeb/src/java/Controlador/ControladorCliente.java
+++ sistemaVentasWeb/src/java/Controlador/ControladorCliente.java
@@ -1,6 +1,9 @@
 package Controlador;
 
+import Modelo.Cliente;
+import Modelo.ClienteDAO;
 import java.io.IOException;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
